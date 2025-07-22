41bbc1568cc7fb8055260cdee5f1326a4b49de75
Johnson Doong <johnsondoong20040608@gmail.com>
2025-07-22 18:36:07 -0400

    Finished random audio selection for the flip button

--- Assets/Scripts/FlipMover.cs
+++ Assets/Scripts/FlipMover.cs
@@ -7,12 +7,19 @@ public class FlipMover : MonoBehaviour
 {
     private bool Moving = false;
     public float speed = 1f;
-    public float direction = 1f;
+    private float direction = 1f;
+    public AudioSource moveClickAudioSource;
+    public AudioSource stopClickAudioSource;
+    public AudioSource flipClickAudioSource;
+
+    public AudioClip stopClickAudioClip;
+
+    public List<AudioClip> flipClickAudioClips;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        stopClickAudioSource.clip = stopClickAudioClip;
     }
 
     // Update is called once per frame
@@ -27,16 +34,27 @@ public class FlipMover : MonoBehaviour
     public void OnMoveClick()
     {
         Moving = true;
+        moveClickAudioSource.Play();
 
     }
 
     public void OnStopClick()
     {
         Moving = false;
+        stopClickAudioSource.Play();
     }
 
     public void OnFlipClick()
     {
         speed *= -1;
+
+        //Take our clips and choose one ramdonly
+
+        //Get a random number from 0 to (the size of the list -1)
+        //Make sure you're taking into account the exclusive nature of the maximum parameter with Random.Range. (min, total + 1)
+        int randomIndex = UnityEngine.Random.Range(0, flipClickAudioClips.Count);
+        AudioClip randomlyChosenClip = flipClickAudioClips[randomIndex];
+
+        flipClickAudioSource.PlayOneShot(randomlyChosenClip);
     }
 }
