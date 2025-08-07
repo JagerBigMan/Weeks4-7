86a750439feaf5fcf792b8e75b0711372e279b90
Johnson Doong <156211561+JagerBigMan@users.noreply.github.com>
2025-08-06 23:19:27 -0400

    Finished Exercise 8

--- Assets/Scripts/ImageEvents.cs
+++ Assets/Scripts/ImageEvents.cs
@@ -2,14 +2,14 @@ using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
-public class ImageEvents : MonoBehaviour
+public class ImageEvents : MonoBehaviour, IPointerClickHandler  //Used this site to figure out how to use the OnClick function https://docs.unity3d.com/Packages/com.unity.ugui@1.0/api/UnityEngine.EventSystems.IPointerClickHandler.html
 {
     public Sprite defaultSprite;
     public Sprite hoverSprite;
     public GameObject prefabToSpawn;
     public GameObject targetObject;
 
-    public void OnPointerEnter(BaseEventData eventData)
+    public void OnPointerEnter(BaseEventData eventData) //I used this site to learn how to use BaseEventData https://stackoverflow.com/questions/68559980/how-to-access-dynamic-baseeventdata-from-c-sharp-script-using-eventtrigger-unit
     {
         Debug.Log("Pointer Entered");
         if (targetObject != null)
@@ -27,9 +27,10 @@ public class ImageEvents : MonoBehaviour
         }
     }
 
-    public void OnPointerClick(BaseEventData eventData)
+    public void OnPointerClick(PointerEventData eventData)      //This method will spawn a falling triangle prefab when clicked on the UI image.
 
     {
+        Debug.Log("Clicked");
         Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         mousePosition.z = 0f;
         Instantiate(prefabToSpawn, mousePosition, Quaternion.identity);
