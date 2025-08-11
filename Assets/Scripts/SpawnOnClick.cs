afe27737606a2179ffc86486375add81070840c0
Johnson Doong <156211561+JagerBigMan@users.noreply.github.com>
2025-08-11 08:31:54 -0400

    Click to spawn script finished
    It spawns a tower at the mouseposition when clicked

---
+++ Assets/Scripts/SpawnOnClick.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnOnClick : MonoBehaviour
+{
+    public GameObject buildingPrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition); 
+            mousePosition.z = 0;
+            Instantiate(buildingPrefab, mousePosition, Quaternion.identity);
+        }
+    }
+}
